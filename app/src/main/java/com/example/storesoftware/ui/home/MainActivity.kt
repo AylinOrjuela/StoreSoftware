2bdfc61651a6c8ade92959c0cf91ee0704ce24ca
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2024-04-16 19:13:58 -0500

    solving problems

--- app/src/main/java/com/example/storesoftware/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/storesoftware/ui/home/MainActivity.kt
@@ -5,13 +5,8 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.storesoftware.databinding.ActivityMainBinding
-<<<<<<< HEAD
-import com.example.storesoftware.ui.shopping.Consult.ConsultPurchaseReceiptViewModel
-import com.example.storesoftware.ui.shopping.Consult.ConsultPurchaseReceiptsActivity
-import com.example.storesoftware.ui.shopping.Register.CreatePurchasesActivity
-=======
 import com.example.storesoftware.ui.product.HomeProduct.MainProductActivity
->>>>>>> d6c110ea9524d3bd1a459357f5696d45cd938b9c
+import com.example.storesoftware.ui.shopping.Consult.ConsultPurchaseReceiptsActivity
 import com.example.storesoftware.ui.user.ConsultUserActivity
 import com.example.storesoftware.ui.user.LogInActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,7 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,35 +25,36 @@ class MainActivity : AppCompatActivity() {
         initUI(user.toString())
     }
 
-    private fun initUI(user:String) {
+    private fun initUI(user: String) {
         initListeners(user)
     }
 
-    private fun initListeners(user:String) {
+    private fun initListeners(user: String) {
         binding.user.setOnClickListener {
             val intent = Intent(this, ConsultUserActivity::class.java)
             intent.putExtra("userId", user)
             startActivity(intent)
             finish()
         }
-<<<<<<< HEAD
-        binding.btnShopping.setOnClickListener {
-            val intent = Intent(this, ConsultPurchaseReceiptsActivity::class.java)
-=======
+
         binding.btnProduct.setOnClickListener {
             val intent = Intent(this, MainProductActivity::class.java)
->>>>>>> d6c110ea9524d3bd1a459357f5696d45cd938b9c
             intent.putExtra("userId", user)
             startActivity(intent)
             finish()
         }
-<<<<<<< HEAD
-=======
-        binding.ivBack.setOnClickListener{
+
+        binding.ivBack.setOnClickListener {
             val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
             finish()
         }
->>>>>>> d6c110ea9524d3bd1a459357f5696d45cd938b9c
+
+        binding.btnShopping.setOnClickListener {
+            val intent = Intent(this, ConsultPurchaseReceiptsActivity::class.java)
+            intent.putExtra("userId", user)
+            startActivity(intent)
+            finish()
+        }
     }
 }
